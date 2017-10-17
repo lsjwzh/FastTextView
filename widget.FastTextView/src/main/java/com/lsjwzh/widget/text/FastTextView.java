411046b2b9d78cce29d7fc7987b559dad1fa7069
lsjwzh <lsjwzh@gmail.com>
2017-10-17 20:43:28 +0800

    init commit

--- widget.FastTextView/src/main/java/com/lsjwzh/widget/text/FastTextView.java
+++ widget.FastTextView/src/main/java/com/lsjwzh/widget/text/FastTextView.java
@@ -5,14 +5,23 @@ import android.graphics.Canvas;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.text.Layout;
+import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 
 /**
  * Simple and Fast TextView.
  */
 public class FastTextView extends FastTextLayoutView {
   private static final String TAG = FastTextView.class.getSimpleName();
+  private CharSequence mText;
+  private TextPaint mTextPaint = new TextPaint();
+
 
   public FastTextView(Context context) {
     super(context);
@@ -51,11 +60,51 @@ public class FastTextView extends FastTextLayoutView {
     }
   }
 
-  public void setText(CharSequence text) {
+  public TextPaint getTextPaint() {
+    return mTextPaint;
+  }
 
+  public void setText(CharSequence text) {
+    if (TextUtils.isEmpty(text)) {
+      mText = "";
+      setTextLayout(null);
+      return;
+    }
+    if (mText != text) {
+      int width;
+      if (text instanceof Spanned) {
+        width = (int) Math.ceil(Layout.getDesiredWidth(text, mTextPaint));
+      } else {
+        width = (int) Math.ceil(mTextPaint.measureText(text, 0, text.length()));
+      }
+      StaticLayout layout = new StaticLayout(text, mTextPaint,
+          width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, true);
+      setTextLayout(layout);
+    }
+    mText = text;
+  }
+
+  public CharSequence getText() {
+    return mText;
   }
 
   public void setTextSize(float textSize) {
+    setTextSize(textSize, TypedValue.COMPLEX_UNIT_SP);
+  }
 
+
+  /**
+   * Set the default text size to a given unit and value.  See {@link
+   * TypedValue} for the possible dimension units.
+   *
+   * @param textSize The desired size in the given units.
+   * @param unit The desired dimension unit.
+   *
+   * @attr ref android.R.styleable#TextView_textSize
+   */
+  public void setTextSize(float textSize, int unit) {
+    float rawTextSize = TypedValue.applyDimension(
+        unit, textSize, getResources().getDisplayMetrics());
+    mTextPaint.setTextSize(rawTextSize);
   }
 }
